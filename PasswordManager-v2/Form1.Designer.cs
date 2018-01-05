0ee4f9434b053e84ad6a3d9eb98313021ed762d9
Yvonne <Yvonne@VVOLF>
2018-01-05 12:25:00 +1100

    Adding in the starting components.

--- PasswordManager-v2/Form1.Designer.cs
+++ PasswordManager-v2/Form1.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ServerName = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.serverbx = new System.Windows.Forms.TextBox();
+            this.userbx = new System.Windows.Forms.TextBox();
+            this.passbx = new System.Windows.Forms.TextBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ServerName
+            // 
+            this.ServerName.AutoSize = true;
+            this.ServerName.Location = new System.Drawing.Point(35, 39);
+            this.ServerName.Name = "ServerName";
+            this.ServerName.Size = new System.Drawing.Size(69, 13);
+            this.ServerName.TabIndex = 2;
+            this.ServerName.Text = "Server Name";
+            this.ServerName.Click += new System.EventHandler(this.ServerName_Click);
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(35, 85);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(55, 13);
+            this.Username.TabIndex = 3;
+            this.Username.Text = "Username";
+            this.Username.Click += new System.EventHandler(this.Username_Click);
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(35, 131);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Password";
+            this.Password.Click += new System.EventHandler(this.Password_Click);
+            // 
+            // serverbx
+            // 
+            this.serverbx.Location = new System.Drawing.Point(129, 36);
+            this.serverbx.Name = "serverbx";
+            this.serverbx.Size = new System.Drawing.Size(100, 20);
+            this.serverbx.TabIndex = 5;
+            this.serverbx.TextChanged += new System.EventHandler(this.serverbx_TextChanged);
+            // 
+            // userbx
+            // 
+            this.userbx.Location = new System.Drawing.Point(129, 82);
+            this.userbx.Name = "userbx";
+            this.userbx.Size = new System.Drawing.Size(100, 20);
+            this.userbx.TabIndex = 6;
+            this.userbx.TextChanged += new System.EventHandler(this.userbx_TextChanged);
+            // 
+            // passbx
+            // 
+            this.passbx.Location = new System.Drawing.Point(129, 128);
+            this.passbx.Name = "passbx";
+            this.passbx.Size = new System.Drawing.Size(100, 20);
+            this.passbx.TabIndex = 7;
+            this.passbx.TextChanged += new System.EventHandler(this.passbx_TextChanged);
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(105, 189);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 8;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.passbx);
+            this.Controls.Add(this.userbx);
+            this.Controls.Add(this.serverbx);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.ServerName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ServerName;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox serverbx;
+        private System.Windows.Forms.TextBox userbx;
+        private System.Windows.Forms.TextBox passbx;
+        private System.Windows.Forms.Button Submit;
     }
 }
 
--- PasswordManager-v2/Form1.cs
+++ PasswordManager-v2/Form1.cs
@@ -16,5 +16,44 @@ namespace PasswordManager_v2
         {
             InitializeComponent();
         }
+
+        public static string SetValueForServerName = "";
+        public static string SetValueForUserName = "";
+        public static string SetValueForPw = "";
+
+        private void ServerName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serverbx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userbx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passbx_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
